64aba4dba6dee83b922fad67f8077a29fe0f0919
RedouxG <chlebojad003@gmail.com>
2023-07-17 14:25:47 +0200

    refactor

--- src/main/global/global.h
+++ src/main/global/global.h
@@ -41,12 +41,6 @@
 #define BYTE_SIZE sizeof(u8)
 #define BYTE_MAX_VALUE 255
 
-#ifdef _MSC_VER
-#define THIS_FUNCTION __FUNCSIG__
-#else
-#define THIS_FUNCTION __PRETTY_FUNCTION__
-#endif
-
 // -------------------------------------------------
 // GLOBAL TYPEDEFS
 // -------------------------------------------------
