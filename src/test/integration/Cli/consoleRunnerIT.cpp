197c4977c49de974a0b88af5d92e6273530e889a
RedouxG <chlebojad003@gmail.com>
2023-08-07 22:31:53 +0200

    writing tests

--- src/test/integration/Cli/consoleRunnerIT.cpp
+++ src/test/integration/Cli/consoleRunnerIT.cpp
@@ -19,7 +19,7 @@ namespace
         (char *)outputFilePath.c_str(),
         (char *)"-fp",
         (char *)passwordFilePath.c_str()};
-    
+
     ConsoleRunner consoleRunner(argc, argv);
     consoleRunner.run();
   }
@@ -40,7 +40,7 @@ namespace
         (char *)outputFilePath.c_str(),
         (char *)"-fp",
         (char *)passwordFilePath.c_str()};
-    
+
     ConsoleRunner consoleRunner(argc, argv);
     consoleRunner.run();
   }
@@ -53,11 +53,32 @@ struct ConsoleRunnerIT : public ::testing::Test
   virtual void TearDown() override {}
 };
 
-TEST_F(ConsoleRunnerIT, test_crypt_and_decrypt_file)
+TEST_F(ConsoleRunnerIT, test_crypt_and_decrypt_file_small)
 {
   // Given
+  const std::filesystem::path passwordFilePath = "passwordFile";
+  const std::filesystem::path sourceFilePath = "sourceFile";
+  const std::filesystem::path cryptedFilePath = "cryptedFile";
+  const std::filesystem::path decryptedFilePath = "decryptedFile";
+
+  const std::vector<BYTE> password = Encryption::get_random_bytes(
+      Global::get_random_u64(4, Encryption::KEY_BYTE_SIZE));
+  const std::vector<BYTE> sourceBytes = Encryption::get_random_bytes(
+      Global::get_random_u64(1, Encryption::KEY_BYTE_SIZE));
 
   // When
+  const bool createdPasswordFile = FileAccess::create_file(passwordFilePath);
+  const FileAccess::ErrorCode appendedPasswordBytes =
+      FileAccess::append_bytes_to_file(passwordFilePath, password);
+  const bool createdSourceFile = FileAccess::create_file(sourceFilePath);
+  const FileAccess::ErrorCode appendedSourceBytes =
+      FileAccess::append_bytes_to_file(sourceFilePath, sourceBytes);
+  
+  
 
   // Then
+  ASSERT_TRUE(createdPasswordFile);
+  ASSERT_EQ(FileAccess::ErrorCode::OK, appendedPasswordBytes);
+  ASSERT_TRUE(createdSourceFile);
+  ASSERT_EQ(FileAccess::ErrorCode::OK, appendedSourceBytes);
 }
